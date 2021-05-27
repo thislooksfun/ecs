3a7cd055e84760cf55aed964ba0061ccfc56f30a
thislooksfun <tlf@thislooks.fun>
2021-05-26 21:40:03 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ECS
+
+This project is in very early alpha. There are no tutorials yet.
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import preprocess from "svelte-preprocess";
+import node from "@sveltejs/adapter-node";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: preprocess(),
+
+  kit: {
+    adapter: node(),
+  },
+};
+
+export default config;
