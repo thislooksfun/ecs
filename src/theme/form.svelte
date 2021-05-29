6585c2b95e38d59ce0687f48611df1b570498c49
thislooksfun <tlf@thislooks.fun>
2021-05-29 15:13:28 -0500

    feat(theme): start theming forms

---
+++ src/theme/form.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let submit: () => void;
+</script>
+
+<form on:submit|preventDefault={submit}>
+  <slot />
+</form>
+
+<style lang="less">
+  form {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
+  form > :global(:not(:first-child)) {
+    margin-top: 1.5em;
+  }
+</style>
