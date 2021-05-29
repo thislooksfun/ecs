56143377007daca1b00fe8963706d76d17701ede
thislooksfun <tlf@thislooks.fun>
2021-05-29 16:14:37 -0500

    feat: add login form to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,21 @@
+<script lang="ts">
+  import AuthModal from "$components/auth/authModal.svelte";
+  import { post } from "$lib/util";
+  import { session, updateSession } from "$lib/clientUtil";
+
+  async function logout() {
+    await post("/api/v1/auth/signout", {});
+    updateSession(s => (s.loggedIn = false));
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+{#if $session.loggedIn}
+  <button on:click={logout}>Log Out</button>
+{:else}
+  <AuthModal />
+{/if}
