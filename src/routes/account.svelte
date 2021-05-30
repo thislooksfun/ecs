0a16b399df370b38347381acef68575dad1aff64
thislooksfun <tlf@thislooks.fun>
2021-05-30 16:43:18 -0500

    feat: add a very basic account viewing page

---
+++ src/routes/account.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" context="module">
+  import type { Load } from "@sveltejs/kit";
+  import { get } from "$lib/util";
+
+  export const load: Load = async ({ fetch }) => {
+    const me = await get(fetch, "/api/v1/me");
+    return { props: { me } };
+  };
+</script>
+
+<script lang="ts">
+  interface Account {
+    email: string;
+  }
+
+  export let me: Account;
+</script>
+
+<h1>Your Account</h1>
+
+<!-- TODO: allow changing email -->
+<span>email: {me.email}</span>
