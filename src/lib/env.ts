67d554a0b63efeedfafa0e9727e533a83868a2c7
thislooksfun <tlf@thislooks.fun>
2021-06-02 10:42:30 -0500

    refactor: fix the DatabaseUrl type

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,5 +2,5 @@ import { config } from "dotenv";
 config();
 
 // Using string keys pending vitejs/vite#3176
-export const DatabaseUrl = process.env["DATABASE_URL"];
+export const DatabaseUrl = process.env["DATABASE_URL"]!;
 export const CookieSecret = process.env["COOKIE_SECRET"]!;
