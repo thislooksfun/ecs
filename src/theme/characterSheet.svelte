b0ff57b99ba7917238fcc182c5a4dbf140b67b76
thislooksfun <tlf@thislooks.fun>
2021-08-23 00:07:02 -0500

    feat: add a basic character sheet template
    This serves as a single universal template for all the character
    sheets. Again, quite a ways before this is complete.

---
+++ src/theme/characterSheet.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  export let name: string;
+  export let avatarUrl: string | undefined = undefined;
+</script>
+
+<div class="sheet">
+  <div class="info">
+    {#if avatarUrl}
+      <img class="avatar" src={avatarUrl} alt="{name}'s avatar" />
+    {/if}
+    <span class="name">{name}</span>
+  </div>
+
+  <div class="health block-fullwidth">
+    <slot name="health" />
+  </div>
+
+  <div class="blocks">
+    <slot />
+  </div>
+</div>
+
+<style lang="less">
+  .sheet {
+    padding: 0 1em;
+  }
+
+  .health {
+    position: relative;
+    width: 100%;
+
+    margin-bottom: 1em;
+  }
+
+  .blocks {
+    display: flex;
+    gap: 1em;
+
+    flex-flow: row wrap;
+    justify-content: center;
+  }
+</style>
