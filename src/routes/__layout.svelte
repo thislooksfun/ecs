a8fa5935f37419444e9ae37debdfb0686ff9cb25
thislooksfun <tlf@thislooks.fun>
2021-08-23 00:16:50 -0500

    feat: add the nav to the main layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,56 @@
 <script lang="ts">
   import Theme from "$theme/theme.svelte";
+  import Nav from "$components/nav.svelte";
+
+  let showNav = true;
 </script>
 
 <Theme />
 
-<slot />
+<div class="layout" class:show-nav={showNav}>
+  <div class="nav-container">
+    <Nav />
+  </div>
+
+  <div class="body-container">
+    <slot />
+  </div>
+</div>
+
+<style lang="less">
+  .layout {
+    position: relative;
+    height: 100%;
+  }
+
+  // Narrow (mobile) layout
+  .nav-container {
+    display: none;
+    position: relative;
+    height: 100%;
+  }
+
+  .show-nav {
+    .nav-container {
+      display: block;
+      background-color: red;
+    }
+  }
+
+  // Wide (desktop) layout
+  @media (min-width: 500px) {
+    .layout {
+      display: grid;
+
+      .nav-container {
+        display: block;
+        background-color: orange;
+      }
+
+      .body-container {
+        position: inherit;
+        // background-color: aquamarine;
+      }
+    }
+  }
+</style>
